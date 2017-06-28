b2c4346845da6b8e2168d9d086c6fb0fbeaaf4ac
lomo44 <vamj44@gmail.com>
2017-06-28 00:08:07 -0400

    - Add web scraper for marvel hero data

---
+++ marvel_wikia_module.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request,urlopen
+from enum import Enum
+import pprint
+class marvel_hero_name(Enum):
+    marvel_hero_iron_man = 0
+    marvel_hero_captain_america = 1
+
+def get_marvel_basic_info(hero_name:marvel_hero_name):
+    marvel_url = ''
+    if hero_name == marvel_hero_name.marvel_hero_iron_man:
+        marvel_url = "http://marvel.wikia.com/wiki/Anthony_Stark_(Earth-616)"
+    elif hero_name == marvel_hero_name.marvel_hero_captain_america:
+        marvel_url = "http://marvel.wikia.com/wiki/Steven_Rogers_(Earth-616)"
+    return_dict = {}
+    if marvel_url != '':
+        hdr = {'User-Agent': 'Mozilla/5.0'}
+        req = Request(marvel_url, headers=hdr)
+        page = urlopen(req)
+        soup = BeautifulSoup(page.read(),"html.parser")
+        l2_f = soup.find_all('div', {'class': 'pi-item pi-data pi-item-spacing pi-border-color'})
+        return_dict = {}
+        for item in l2_f:
+            label = item.find('h3', {'class': 'pi-data-label pi-secondary-font'}).text
+            value = item.find('div', {'class': 'pi-data-value pi-font'})
+            sub_tag = value.find_all('sup')
+            for tag in sub_tag:
+                tag.extract()
+            if label not in return_dict:
+                return_dict[label.strip()] = []
+            return_dict[label.strip()] += list(map(lambda x:x.strip(),value.text.strip().split(',')))
+    return return_dict
+
+
+if __name__ == "__main__":
+    iron_man_info = get_marvel_basic_info(marvel_hero_name.marvel_hero_iron_man)
+    pprint.pprint(iron_man_info,indent=4)
---
+++ test_marvel_wikia_module.py
@@ -0,0 +1,25 @@
+import unittest
+from marvel_wikia_module import get_marvel_basic_info,marvel_hero_name
+
+
+class test_marvel_wikia_module(unittest.TestCase):
+    def test_unknown_type(self):
+        iron_man_info = get_marvel_basic_info(0)
+        self.assertIsInstance(iron_man_info, dict)
+        self.assertDictEqual(iron_man_info, {})
+
+    def test_iron_man(self):
+        iron_man_info = get_marvel_basic_info(marvel_hero_name.marvel_hero_iron_man)
+        self.assertIsInstance(iron_man_info,dict)
+        self.assertNotEqual(iron_man_info,{})
+        self.assertListEqual(['Anthony Edward "Tony" Stark'],iron_man_info['Real Name'])
+
+    def test_captain_america(self):
+        iron_man_info = get_marvel_basic_info(marvel_hero_name.marvel_hero_captain_america)
+        self.assertIsInstance(iron_man_info,dict)
+        self.assertNotEqual(iron_man_info,{})
+        self.assertListEqual(['Steven "Steve" Grant Rogers'],iron_man_info['Real Name'])
+
+
+if __name__ == "__main__":
+    unittest.main()
