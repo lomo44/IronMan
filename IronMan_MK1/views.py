ff57d4df83d340e68df55a56a8b8c7f0c9c4e9be
lomo44 <vamj44@gmail.com>
2017-11-09 13:25:02 -0500

    Fix None text message handling

--- IronMan_MK1/views.py
+++ IronMan_MK1/views.py
@@ -48,5 +48,8 @@ class fb_echo(generic.View):
                 if 'message' in message:
                     # Print the message to the terminal
                     pprint(message)
-                    post_fb_messager_msg(message['sender']['id'], message['message']['text'])
+                    if 'text' in mesasge['mssage']:
+                        post_fb_messager_msg(message['sender']['id'], message['message']['text'])
+                    else
+                        post_fb_messager_msg(message['sender']['id'], "Image/Gif support is limited")
         return HttpResponse()
