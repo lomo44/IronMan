5054aeee1336e9f3e8822f5edf66c3d40fc43818
MichelleL <user@michelles-MacBook-Pro.local>
2017-12-02 16:15:15 -0500

    ACTUALLY Disablling debug flag (forgot to do git add in the previous commit)

--- IronMan/settings.py
+++ IronMan/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'trgv7&h$jl$g17=5)6=s^f8#@b61=#j)nkz_^2o^v@mi*_rd=$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "1c8b33f3.ngrok.io",
