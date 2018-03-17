906a76dababa2df5b8c333575830befa8f143bb9
lomo44 <vamj44@gmail.com>
2018-03-16 20:38:53 -0400

    fix dependency problem

--- IronMan_MK1/modules/base/base_module.py
+++ IronMan_MK1/modules/base/base_module.py
@@ -1,12 +1,10 @@
 
-from IronMan_MK1.modules.base.base_packet import Base_packet
-
 
 class Base_module(object):
     def __init__(self):
         pass
 
-    def process(self, _input : Base_packet) -> Base_packet:
+    def process(self, _input ):
         return NotImplemented
 
 
--- IronMan_MK1/modules/nlu/IM_NLU_Module.py
+++ IronMan_MK1/modules/nlu/IM_NLU_Module.py
@@ -1,8 +1,6 @@
-from modules.Base_module import Base_module
-from modules.Module_packet import Module_packet,eModule_packet_flag
+from IronMan_MK1.modules.base.base_module import Base_module
+
 
 class IM_NLU_Module(Base_module):
-    def process(self, _input : Module_packet):
-        if _input.flag == eModule_packet_flag.eModule_packet_flag_test:
-            _input.payload["output_payload"].append("IM_NLU_Module")
+    def process(self, _input ):
         return _input
